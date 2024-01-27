695a7866f293cc005c132e78c200639426f9b046
Austin Dlugosch <austin@dlugosch-family.com>
2024-01-27 09:59:01 -0600

    CubeFace is now case iterable

--- Sources/LevelGeneration/CubeFace.swift
+++ Sources/LevelGeneration/CubeFace.swift
@@ -1,4 +1,4 @@
-public enum CubeFace: Int, Hashable { // Represents a face on the cube, associated values correspond to the order in which faces appear in an array    
+public enum CubeFace: Int, Hashable, CaseIterable { // Represents a face on the cube, associated values correspond to the order in which faces appear in an array    
     case back = 0
     case left = 1
     case top = 2
